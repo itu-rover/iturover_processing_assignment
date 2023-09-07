8530ceed706677280d4ea5f3fc32b6a289bf5099
yerenkl <yerenkl@gmail.com>
2023-09-07 20:44:12 +0300

    first init

---
+++ src/filter_publisher.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# @author yerenkl
+# Date: 05/09/2023
+
+import rospy
+from std_msgs.msg import String
+import random
+
+class itu_rover_node:
+    def __init__(self):
+        rospy.init_node('itu_rover_node', anonymous=True)
+        self.filter_publisher = rospy.Publisher('/filter', String, queue_size=10)
+        self.filters = ["GRAY", "RESIZE_UP", "RESIZE_DOWN", "RGB"]
+        self.rate = rospy.Rate(0.5)  
+    def run(self):
+        while not rospy.is_shutdown():
+            random_filter = random.choice(self.filters)
+
+            self.filters.remove(random_filter)      
+            
+            rospy.loginfo("Publishing: %s", random_filter)
+            self.filter_publisher.publish(random_filter)
+
+            if not self.filters:
+                self.filters=["GRAY", "RESIZE_UP", "RESIZE_DOWN", "RGB"]
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        node = itu_rover_node()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
